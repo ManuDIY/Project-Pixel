930f3db59f72dcf0b6005413fc43a272a37e14a8
intensity412 <42899502+intensity412@users.noreply.github.com>
2020-01-10 18:03:51 -0500

    Remove Chukwa code

--- src/crypto/hash.h
+++ src/crypto/hash.h
@@ -9,7 +9,6 @@
 
 #include <stddef.h>
 
-#include "argon2.h"
 
 #include <CryptoTypes.h>
 
@@ -46,12 +45,6 @@
 #error The CryptoNight Soft Shell Parameters you supplied will exceed normal paging operations.
 #endif
 
-// Chukwa Definitions
-#define CHUKWA_HASHLEN 32 // The length of the resulting hash in bytes
-#define CHUKWA_SALTLEN 16 // The length of our salt in bytes
-#define CHUKWA_THREADS 1 // How many threads to use at once
-#define CHUKWA_ITERS 3 // How many iterations we perform as part of our slow-hash
-#define CHUKWA_MEMORY 512 // This value is in KiB (0.5MB)
 
 namespace Crypto {
 
@@ -59,7 +52,6 @@ namespace Crypto {
 #include "hash-ops.h"
   }
 	
-  static bool argon2_optimization_selected = false;
 	
   /*
     Cryptonight hash functions
@@ -183,26 +175,6 @@ namespace Crypto {
 
     cn_slow_hash(data, length, reinterpret_cast<char *>(&hash), 1, 1, 0, pagesize, scratchpad, iterations);
   }
-  inline void chukwa_slow_hash(const void *data, size_t length, Hash &hash)
-    {
-        uint8_t salt[CHUKWA_SALTLEN];
-        memcpy(salt, data, sizeof(salt));
-
-        /* If this is the first time we've called this hash function then
-           we need to have the Argon2 library check to see if any of the
-           available CPU instruction sets are going to help us out */
-        if (!argon2_optimization_selected)
-        {
-            /* Call the library quick benchmark test to set which CPU
-               instruction sets will be used */
-            argon2_select_impl(NULL, NULL);
-
-            argon2_optimization_selected = true;
-        }
-
-        argon2id_hash_raw(
-            CHUKWA_ITERS, CHUKWA_MEMORY, CHUKWA_THREADS, data, length, salt, CHUKWA_SALTLEN, hash.data, CHUKWA_HASHLEN);
-    }
 
   inline void cn_soft_shell_slow_hash_v2(const void *data, size_t length, Hash &hash, uint32_t height) {
     uint32_t base_offset = (height % CN_SOFT_SHELL_WINDOW);
